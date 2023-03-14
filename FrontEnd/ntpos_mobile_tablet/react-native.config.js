28df9824df5d4a684e9a31377a628ea8fefe64db
NGUYEN LE THANH <forenamethanh@gmail.com>
2023-03-14 08:21:48 +0700

    update mobile base: login, register, routes

---
+++ FrontEnd/ntpos_mobile_tablet/react-native.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: ['./src/assets/'],
+  dependencies: {
+    'react-native-vector-icons': {
+      platforms: {
+        ios: null,
+      },
+    },
+  },
+};
